f091279ed698af00633bae1d62b7b1504f0bdab6
masahitojp <randomstep@gmail.com>
2018-03-20 00:45:27 +0900

    :sparkles: first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from './Container';
+import Header from './Header';
+
+const App = () => (
+  <Container>
+    <Header>Title</Header>
+  </Container>
+);
+
+export default App;
---
+++ src/Container.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.article`
+  background: #F6F6F6;
+  width: 100%;
+  height: 100vh;
+`;
+
+export default Container;
